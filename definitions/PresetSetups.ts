63d4bd162ee782a1c2fbdd55b9818d99541c1b4d
Pham Ngoc Duy <phamngocduy98@gmail.com>
2020-10-01 22:38:53 +0700

    fix lint, fix wrong import for PreSetups_auto.ts

--- definitions/PresetSetups.ts
+++ definitions/PresetSetups.ts
@@ -1,4 +1,4 @@
 export const MAX_PLAYER_IN_GAME = 20;
 export const MIN_PLAYER_IN_GAME = 4;
 
-export * from "../utils/create_setup_tool/AutoGenerateSetupV2";
+export * from "../utils/create_setup_tool/PreSetups_auto";
