dc4b673aa501ac39e3da894c60187c379b1c03be
linhtq <tranquanglinh.pt@gmai.com>
2020-10-03 22:31:54 +0700

    update setup error enum

--- colyseus/RoomErrors.ts
+++ colyseus/RoomErrors.ts
@@ -3,3 +3,15 @@ export enum RoomErrors {
     ROOM_IS_FULL = "ROOM_IS_FULL",
     WRONG_ROOM_PASSWORD = "WRONG_ROOM_PASSWORD"
 }
+
+export enum SetupErrorTypes {
+    PLAYERS_NOT_FIT = "setupNotFitToPlayers",
+    NOT_ENOUGH_WEREWOLF = "setupHaveNoWolf",
+    TOO_MUCH_WEREWOLF = "setupHaveTooMuchWolf",
+    ROLE_REQUIRE_NOT_FIT = "setupHaveRoleRequireRole"
+}
+
+export interface ISetupError {
+    type: SetupErrorTypes;
+    data: string[];
+}
