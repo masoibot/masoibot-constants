4c6df21cc52f33eb9741864b4aefc6571b23983f
linhtq <tranquanglinh.pt@gmail.com>
2020-10-11 13:57:18 +0700

    chỉnh EventData

--- definitions/EventData.ts
+++ definitions/EventData.ts
@@ -4,8 +4,8 @@ export class EventData {
     constructor(
         public targets: string[] = [],
         public success: boolean = true,
-        public dayNo: number = undefined,
-        public session: number = undefined
+        public dayNo: number|undefined = undefined,
+        public session: number|undefined = undefined
     ) {}
 }
 
@@ -14,31 +14,3 @@ export class CupidEventData extends EventData {
         super(targets, true);
     }
 }
-
-export const EventDataWithSkill: {[eventName in EventNames]: {new (): EventData}} = {
-    [EventNames.ADMIN_START_GAME]: EventData,
-    [EventNames.BECOME_WOLF]: EventData,
-    [EventNames.BE_CURSED]: EventData,
-    [EventNames.BE_PAIRED_WITH]: EventData,
-    [EventNames.BITE]: EventData,
-    [EventNames.CHOOSE_MOTHER]: EventData,
-    [EventNames.CURSE]: EventData,
-    [EventNames.DIE_OF_LOVE]: EventData,
-    [EventNames.FIRE]: EventData,
-    [EventNames.FIRE_DIRECTLY]: EventData,
-    [EventNames.KILL]: EventData,
-    [EventNames.NO_EVENT]: EventData,
-    [EventNames.PAIRING]: EventData,
-    [EventNames.POINT]: EventData,
-    [EventNames.PROTECT]: EventData,
-    [EventNames.READY]: EventData,
-    [EventNames.SAVE]: EventData,
-    [EventNames.SEE]: EventData,
-    [EventNames.SEE_MOTHER_DIE]: EventData,
-    [EventNames.SKIP]: EventData,
-    [EventNames.VIEW_DEAD_MAN]: EventData,
-    [EventNames.BE_BITE]: EventData,
-    [EventNames.VOTE]: EventData,
-    [EventNames.VOTE_NO]: EventData,
-    [EventNames.VOTE_YES]: EventData
-};
