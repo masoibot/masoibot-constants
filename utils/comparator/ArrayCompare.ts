e32da313ccc38716962f86f695a1efc99b636b94
Pham Ngoc Duy <phamngocduy98@gmail.com>
2020-11-01 19:01:47 +0700

    add array comparator

---
+++ utils/comparator/ArrayCompare.ts
@@ -0,0 +1,7 @@
+export function is2ArrayEqual<T>(a: T[], b: T[]) {
+    if (a.length !== b.length) return false;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
